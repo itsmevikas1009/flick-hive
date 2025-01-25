1d2ac7714bda2f58b1a6c4e3ff0da45a912405ec
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2025-01-25 11:14:36 +0530

    Add comments and improve documentation in health check controller

--- server/src/controllers/healthCheck.controller.js
+++ server/src/controllers/healthCheck.controller.js
@@ -1,8 +1,10 @@
+// Import necessary utilities
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-
+// Function to perform a health check
 const healthCheck = asyncHandler(async (req, res) => {
+    // Return a successful response with status 200 and message 'OK'
     return res
         .status(200)
         .json(
@@ -14,4 +16,5 @@ const healthCheck = asyncHandler(async (req, res) => {
         );
 });
 
+// Export the healthCheck function
 export { healthCheck };
