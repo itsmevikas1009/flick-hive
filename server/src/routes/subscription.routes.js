adb73badf3aadf69ce01fb73737339ba5b980498
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2025-02-19 18:28:14 +0530

    `feat: add route for retrieving subscribed channels by subscriber ID`

--- server/src/routes/subscription.routes.js
+++ server/src/routes/subscription.routes.js
@@ -11,11 +11,15 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/c/:channelId")
-    .get(getSubscribedChannels)
     .post(toggleSubscription);
 
+
 router
-    .route("/u/:subscriberId")
+    .route("/c/:subscriberId")
+    .get(getSubscribedChannels);
+
+router
+    .route("/u/:channelId")
     .get(getUserChannelSubscribers);
 
 export default router
