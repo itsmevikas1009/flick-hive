f3ed2efd9060a7b10fbc93cf6eee807961d21d77
Vikas Chaurasia <chaurasiavikas100904@gmail.com>
2025-02-19 18:39:12 +0530

    `fix: update dashboard controller to retrieve channel stats and videos using authenticated user ID`

--- server/src/controllers/dashboard.controller.js
+++ server/src/controllers/dashboard.controller.js
@@ -10,7 +10,7 @@ import { User } from "../models/user.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    const channel = await User.findById(req.params.channelId);
+    const channel = await User.findById(req.user._id);
     if (!channel) {
         throw new ApiError(404, "Channel not found");
     }
@@ -38,11 +38,11 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    const channel = await User.findById(req.params.channelId);
+    const channel = await User.findById(req.user._id);
     if (!channel) {
         throw new ApiError(404, "Channel not found");
     }
-    const videos = await Video.find({ channel: channel._id });
+    const videos = await Video.find({ owner: channel._id });
     return res.status(200).json(new ApiResponse(200, videos, "Videos retrieved successfully",));
 });
 
